99ba959f9ae09db0e5688f53dcebc1b8d620a838
milestgg <milestran4@gmail.com>
2023-04-06 09:49:56 -0400

    Updated Gui with help screen

--- MusicMakerGUIHalf.py
+++ MusicMakerGUIHalf.py
@@ -10,7 +10,7 @@ class GUI(tk.Tk):
         self.screen1 = tk.Frame(self, bg="lightblue", width=800, height=600)
         self.screen2 = tk.Frame(self, bg="lightblue", width=800, height=600)
         self.screen3 = tk.Frame(self, bg="lightblue", width=800, height=600)
-
+        self.helpscreen = tk.Frame(self, bg="lightblue", width=800, height=600)
 
         self.title_label = tk.Label(self.screen1, text="FluidMusic", fg="black", font=("Arial", 40))
         self.title_label.pack(padx=50, pady=50)
@@ -28,12 +28,16 @@ class GUI(tk.Tk):
 
         self.label3 = tk.Label(self.screen3, text="Screen 3", fg="black", font=("Arial", 30))
         self.label3.pack(padx=50, pady=50)
-
+        
+        self.labelhelp = tk.Label(self.helpscreen, text="Help", fg="black", font=("Arial", 30))
+        self.labelhelp.pack(padx=50, pady=50)
+        self.labelhelp2 = tk.Label(self.helpscreen, text="Select between 10-15 songs that you like and we will give you a custom playlist for you!", fg="black", font=("Arial", 10))
+        self.labelhelp2.pack(padx=50, pady=50)
 
         self.next_button1 = tk.Button(self.screen1, text="Next", fg="white", bg="blue", font=("Arial", 20), command=self.show_screen2)
         self.next_button1.pack(side="bottom")
         #help button
-        self.help_button = tk.Button(self.screen1, text="Help", fg="white", bg="blue", font=("Arial", 20), command=self.show_screen2)
+        self.help_button = tk.Button(self.screen1, text="Help", fg="white", bg="blue", font=("Arial", 20), command=self.show_helpscreen)
         self.help_button.pack(side="bottom")
 
         self.next_button2 = tk.Button(self.screen2, text="Next", fg="white", bg="blue", font=("Arial", 20), command=self.show_screen3)
@@ -46,6 +50,9 @@ class GUI(tk.Tk):
 
         self.back_button2 = tk.Button(self.screen3, text="Back", fg="white", bg="blue", font=("Arial", 20), command=self.show_screen2)
         self.back_button2.pack(side="left")
+        
+        self.back_button3 = tk.Button(self.helpscreen, text="Back", fg="white", bg="blue", font=("Arial", 20), command=self.show_screen1)
+        self.back_button3.pack(side="left")
 
         # Show screen 1
         self.screen1.pack()
@@ -55,18 +62,27 @@ class GUI(tk.Tk):
         self.screen1.pack()
         self.screen2.pack_forget()
         self.screen3.pack_forget()
+        self.helpscreen.pack_forget()
 
     def show_screen2(self):
-       
         self.screen1.pack_forget()
         self.screen2.pack()
         self.screen3.pack_forget()
+        self.helpscreen.pack_forget()
 
     def show_screen3(self):
-
         self.screen1.pack_forget()
         self.screen2.pack_forget()
         self.screen3.pack()
+        self.helpscreen.pack_forget()
         
+    def show_helpscreen(self):
+        # go to help screen
+        self.screen1.pack_forget()
+        self.screen2.pack_forget()
+        self.screen3.pack_forget()
+        self.helpscreen.pack()
+        
+
 gui = GUI()
 gui.mainloop()
